a4acd3c8da3a8901cd2bf4c2e8939a1f3e1e0456
PranamBhat <pranam707@gmail.com>
2020-10-08 23:36:00 +0530

    Committing 3 programs

---
+++ BinaryTriangle/BinaryTriangle/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * C# Program to Print a BinaryTriangle
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BinaryTriangle
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            int p, lastInt = 0, input;
+            Console.WriteLine("Enter the Number of Rows : ");
+            input = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= input; i++)
+            {
+                for (p = 1; p <= i; p++)
+                {
+                    if (lastInt == 1)
+                    {
+                        Console.Write("0");
+                        lastInt = 0;
+                    }
+                    else if (lastInt == 0)
+                    {
+                        Console.Write("1");
+                        lastInt = 1;
+                    }
+                }
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CountTheNumberOfOnes/CountTheNumberOfOnes/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * C# Program to Count the Number of 1's in the Entered Number
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CountTheNumberOfOnes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m, count = 0;
+            Console.WriteLine("Enter the Limit : ");
+            m = int.Parse(Console.ReadLine());
+            int[] a = new int[m];
+            Console.WriteLine("Enter the Numbers :");
+            for (int i = 0; i < m; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            foreach (int o in a)
+            {
+                if (o == 1)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("Number of 1's in the Entered Number : ");
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
